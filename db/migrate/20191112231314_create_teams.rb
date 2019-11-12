e9ec3154d38e07bc00649043b97b0c7b884105b7
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-12 17:17:36 -0600

    resource for teams

---
+++ db/migrate/20191112231314_create_teams.rb
@@ -0,0 +1,11 @@
+class CreateTeams < ActiveRecord::Migration[6.0]
+  def change
+    create_table :teams do |t|
+      t.string :name
+      t.string :region
+      t.integer :season_wins
+      t.integer :season_losses
+      t.timestamps
+    end
+  end
+end
