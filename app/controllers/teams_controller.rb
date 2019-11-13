bc5110ed3feca894054183f6faae87d0338232da
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-12 18:59:00 -0600

    created a before_action for teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,28 +1,42 @@
 class TeamsController < ApplicationController
+    before_action :find_team only: [:show, :edit, :update, :destroy]
+
     def index 
+        
+
     end 
 
     def show
+
     end 
 
     def new
+
     end 
 
     def create
+
     end 
 
-    def edit 
+    def edit
+
     end 
 
     def update
+
     end 
 
     def destroy
+
     end 
 
-    def 
+    private 
+
+    def team_params 
+        params.require(:team).require(:team_name, :region, :season_wins, :season_wins)
     end 
 
-    def
+    def find_params
+        @team = Team.find(params[:id])
     end 
 end
