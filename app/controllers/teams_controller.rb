7b0706039df8ed9530b197d4475a1575f8c52004
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-12 18:52:10 -0600

    started 6 methods for teams controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,2 +1,28 @@
 class TeamsController < ApplicationController
+    def index 
+    end 
+
+    def show
+    end 
+
+    def new
+    end 
+
+    def create
+    end 
+
+    def edit 
+    end 
+
+    def update
+    end 
+
+    def destroy
+    end 
+
+    def 
+    end 
+
+    def
+    end 
 end
