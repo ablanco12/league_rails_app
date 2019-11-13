4e6775435a95d0755bf4678d70786f159abe129f
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-12 19:04:00 -0600

    Filled in methods in Teams Controller

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -2,32 +2,33 @@ class TeamsController < ApplicationController
     before_action :find_team only: [:show, :edit, :update, :destroy]
 
     def index 
-        
-
+        @team = Team.all 
     end 
 
     def show
-
     end 
 
     def new
-
+        @team = Team.new 
     end 
 
     def create
-
+        @team = Team.create(team_params)
+        redirect_to @team 
     end 
 
     def edit
-
     end 
 
     def update
+        @team.update(team_params)
+        redirect_to @team 
 
     end 
 
     def destroy
-
+        @team.destroy
+        redirect_to @team 
     end 
 
     private 
