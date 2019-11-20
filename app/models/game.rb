79f4479789a291618d7b7f4c9c139e845964efd3
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-19 22:14:58 -0600

    Changing Game model validations to fit the proper associations and changing the new, index and show form to work with game_number attribute.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,7 +1,6 @@
 class Game < ApplicationRecord
     belongs_to :team
     validates :date, presence:true
-    validates :team_id, presence:{message: "needs to be filled in!"}
-    # accepts_nested_attributes_for :teams
-
+    # validates :team_id, presence:{message: "needs to be filled in!"}
+    validates :game_number, uniqueness:true 
 end
