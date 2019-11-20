08cf33b8850cf02fce84edd44d23ed66d9c9f4b7
Guillermo Martinez <dumblemoe@gmail.com>
2019-11-19 18:21:55 -0600

    Creating error message for Game date validation.

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,6 @@
 class Game < ApplicationRecord
     has_many :teams
-    validates :date, presence:{message: "needs to be filled in!"}
-    validates :team_id, presence:true
+    validates :date, presence:true
+    validates :team_id, presence:{message: "needs to be filled in!"}
 
 end
